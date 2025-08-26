e06d8e37221d6f50c99cbb58e2b626f32173dff0
TheBjoRedCraft <thebjoredcraft@gmail.com>
2025-08-26 15:58:03 +0200

    refactor: Simplify methods in FallbackFriendService for improved readability

--- surf-friends-fallback/src/main/kotlin/dev/slne/surf/friends/fallback/service/FallbackFriendService.kt
+++ surf-friends-fallback/src/main/kotlin/dev/slne/surf/friends/fallback/service/FallbackFriendService.kt
@@ -2,21 +2,18 @@ package dev.slne.surf.friends.fallback.service
 
 import com.google.auto.service.AutoService
 import dev.slne.surf.friends.api.model.FriendRequest
-import dev.slne.surf.friends.api.model.Friendship
 import dev.slne.surf.friends.core.service.FriendService
 import dev.slne.surf.friends.core.service.databaseService
-import it.unimi.dsi.fastutil.objects.ObjectSet
 import net.kyori.adventure.util.Services
 import java.util.*
 
 @AutoService(FriendService::class)
 class FallbackFriendService : FriendService, Services.Fallback {
-    override suspend fun createFriendship(uuid: UUID, friend: UUID): Friendship {
-        return databaseService.getFriendship(uuid, friend) ?: databaseService.addFriendship(
+    override suspend fun createFriendship(uuid: UUID, friend: UUID) =
+        databaseService.getFriendship(uuid, friend) ?: databaseService.addFriendship(
             uuid,
             friend
         )
-    }
 
     override suspend fun removeFriendship(uuid: UUID, friend: UUID) {
         databaseService.removeFriendship(uuid, friend)
@@ -26,25 +23,18 @@ class FallbackFriendService : FriendService, Services.Fallback {
     override suspend fun getFriendship(
         playerA: UUID,
         playerB: UUID
-    ): Friendship? {
-        return databaseService.getFriendship(playerA, playerB)
-    }
+    ) = databaseService.getFriendship(playerA, playerB)
 
     override suspend fun areFriends(
         uuid: UUID,
         friend: UUID
-    ): Friendship? {
-        return databaseService.getFriendship(uuid, friend)
-    }
+    ) = databaseService.getFriendship(uuid, friend)
 
-    override suspend fun getFriendships(uuid: UUID): ObjectSet<Friendship> {
-        return databaseService.getFriends(uuid)
-    }
+    override suspend fun getFriendships(uuid: UUID) = databaseService.getFriends(uuid)
 
-    override suspend fun sendFriendRequest(sender: UUID, receiver: UUID): FriendRequest {
-        return databaseService.getFriendRequest(sender, receiver)
+    override suspend fun sendFriendRequest(sender: UUID, receiver: UUID): FriendRequest =
+        databaseService.getFriendRequest(sender, receiver)
             ?: databaseService.addFriendRequest(sender, receiver)
-    }
 
     override suspend fun acceptFriendRequest(sender: UUID, receiver: UUID) {
         databaseService.removeFriendRequest(sender, receiver)
@@ -52,28 +42,22 @@ class FallbackFriendService : FriendService, Services.Fallback {
         databaseService.addFriendship(receiver, sender)
     }
 
-    override suspend fun declineFriendRequest(sender: UUID, receiver: UUID) {
+    override suspend fun declineFriendRequest(sender: UUID, receiver: UUID) =
         databaseService.removeFriendRequest(sender, receiver)
-    }
 
-    override suspend fun revokeFriendRequest(sender: UUID, receiver: UUID) {
+    override suspend fun revokeFriendRequest(sender: UUID, receiver: UUID) =
         databaseService.removeFriendRequest(sender, receiver)
-    }
 
-    override suspend fun getSentFriendRequests(uuid: UUID): ObjectSet<FriendRequest> {
-        return databaseService.getSentFriendRequests(uuid)
-    }
+    override suspend fun getSentFriendRequests(uuid: UUID) =
+        databaseService.getSentFriendRequests(uuid)
 
-    override suspend fun getReceivedFriendRequests(uuid: UUID): ObjectSet<FriendRequest> {
-        return databaseService.getReceivedFriendRequests(uuid)
-    }
+    override suspend fun getReceivedFriendRequests(uuid: UUID) =
+        databaseService.getReceivedFriendRequests(uuid)
 
     override suspend fun getFriendRequest(
         sender: UUID,
         target: UUID
-    ): FriendRequest? {
-        return databaseService.getFriendRequest(sender, target)
-    }
+    ) = databaseService.getFriendRequest(sender, target)
 
     override suspend fun toggleAnnouncements(uuid: UUID): Boolean {
         val friendSettings = databaseService.getFriendSettings(uuid)
