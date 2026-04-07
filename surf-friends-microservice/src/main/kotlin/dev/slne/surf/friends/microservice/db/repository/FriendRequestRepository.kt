6119bbbc1acf8d54eaf24e1f3b043dac83138d5a
Ammo <ammo@slne.dev>
2026-04-07 13:54:51 +0200

    feat: Add handling for fetching, revoking, and changing friend request states

--- surf-friends-microservice/src/main/kotlin/dev/slne/surf/friends/microservice/db/repository/FriendRequestRepository.kt
+++ surf-friends-microservice/src/main/kotlin/dev/slne/surf/friends/microservice/db/repository/FriendRequestRepository.kt
@@ -1,5 +1,8 @@
 package dev.slne.surf.friends.microservice.db.repository
 
+import dev.slne.surf.database.libs.org.jetbrains.exposed.v1.core.and
+import dev.slne.surf.database.libs.org.jetbrains.exposed.v1.core.eq
+import dev.slne.surf.database.libs.org.jetbrains.exposed.v1.r2dbc.deleteWhere
 import dev.slne.surf.database.libs.org.jetbrains.exposed.v1.r2dbc.insert
 import dev.slne.surf.database.libs.org.jetbrains.exposed.v1.r2dbc.selectAll
 import dev.slne.surf.database.libs.org.jetbrains.exposed.v1.r2dbc.transactions.suspendTransaction
@@ -7,6 +10,7 @@ import dev.slne.surf.friends.api.model.FriendRequest
 import dev.slne.surf.friends.microservice.db.table.FriendRequestsTable
 import kotlinx.coroutines.flow.map
 import kotlinx.coroutines.flow.toList
+import java.util.*
 
 object FriendRequestRepository {
     suspend fun fetchFriendRequests(): List<FriendRequest> = suspendTransaction {
@@ -26,6 +30,18 @@ object FriendRequestRepository {
             it[this.senderUuid] = friendRequest.senderUuid
             it[this.targetUuid] = friendRequest.targetUuid
             it[this.createdAt] = friendRequest.createdAt
+        }
+
+        Unit
+    }
+
+    suspend fun deleteFriendRequest(
+        senderUuid: UUID,
+        targetUuid: UUID
+    ) = suspendTransaction {
+        FriendRequestsTable.deleteWhere {
+            (FriendRequestsTable.senderUuid eq senderUuid) and
+                    (FriendRequestsTable.targetUuid eq targetUuid)
         }
 
         Unit
--- surf-friends-microservice/src/main/kotlin/dev/slne/surf/friends/microservice/rabbit/handler/FriendRequestHandler.kt
+++ surf-friends-microservice/src/main/kotlin/dev/slne/surf/friends/microservice/rabbit/handler/FriendRequestHandler.kt
@@ -2,14 +2,25 @@ package dev.slne.surf.friends.microservice.rabbit.handler
 
 import dev.slne.surf.friends.api.model.FriendRequest
 import dev.slne.surf.friends.api.result.FriendRequestCreateResult
-import dev.slne.surf.friends.core.common.packets.friendrequest.CreateFriendRequestRequestPacket
-import dev.slne.surf.friends.core.common.packets.friendrequest.CreateFriendRequestResponsePacket
+import dev.slne.surf.friends.api.result.FriendRequestRemoveResult
+import dev.slne.surf.friends.api.result.FriendRequestStateResult
+import dev.slne.surf.friends.core.common.packets.friendrequest.*
 import dev.slne.surf.friends.microservice.db.repository.FriendRequestRepository
+import dev.slne.surf.friends.microservice.db.repository.FriendshipsRepository
 import dev.slne.surf.rabbitmq.api.handler.RabbitHandler
 import kotlinx.coroutines.launch
 import java.time.OffsetDateTime
 
 object FriendRequestHandler {
+    @RabbitHandler
+    fun handleFetchFriendRequestsPacket(
+        request: FetchFriendRequestsRequestPacket
+    ) = request.launch {
+        request.respond(
+            FetchFriendRequestsResponsePacket(FriendRequestRepository.fetchFriendRequests())
+        )
+    }
+
     @RabbitHandler
     fun handleCreateFriendRequestPacket(
         request: CreateFriendRequestRequestPacket
@@ -30,6 +41,36 @@ object FriendRequestHandler {
             CreateFriendRequestResponsePacket(
                 result = FriendRequestCreateResult.Success(friendRequest)
             )
+        )
+    }
+
+    @RabbitHandler
+    fun handleRevokeFriendRequestPacket(
+        request: RevokeFriendRequestRequestPacket
+    ) = request.launch {
+        FriendRequestRepository.deleteFriendRequest(request.senderUuid, request.receiverUuid)
+
+        request.respond(
+            RevokeFriendRequestResponsePacket(FriendRequestRemoveResult.Success)
+        )
+    }
+
+    @RabbitHandler
+    fun handleChangeFriendRequestStatePacket(
+        request: ChangeFriendRequestStateRequestPacket
+    ) = request.launch {
+        FriendRequestRepository.deleteFriendRequest(request.senderUuid, request.targetUuid)
+
+        if (request.state == ChangeFriendRequestStateRequestPacket.State.ACCEPT) {
+            FriendshipsRepository.createFriendship(
+                request.senderUuid,
+                request.targetUuid,
+                OffsetDateTime.now()
+            )
+        }
+
+        request.respond(
+            ChangeFriendRequestStateResponsePacket(FriendRequestStateResult.Success)
         )
     }
 }
